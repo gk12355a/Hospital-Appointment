b54cb078880a1101fe08633700d93236ac5e90ed
KienDoTri <kiendotri@gmail.com>
2025-01-18 00:21:51 +0700

    config: vm

--- WebApp/obj/Release/net6.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Release/net6.0/WebApp.AssemblyInfo.cs
@@ -12,10 +12,11 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("c8f55d62-6dc2-429e-988a-a2bba940a1b8")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17908b640867bedc86deab65f0d03ed9306d3493")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa1876b35acaa8bfd24f74892d1333638eb1b3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
