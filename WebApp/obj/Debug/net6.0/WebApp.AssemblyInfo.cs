e81e9851e13d293fcb71ba8fbe6dcedcd44b6ffb
nguyenlee24 <kiendotri@gmail.com>
2025-01-17 18:16:46 +0000

    config: add dockerfile

--- WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa1876b35acaa8bfd24f74892d1333638eb1b3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b54cb078880a1101fe08633700d93236ac5e90ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
